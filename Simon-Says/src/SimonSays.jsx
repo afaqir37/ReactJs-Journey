4b0a6887ab2ebfed6b05eae3bbf0d29d5c28e56c
Faqir Abdellah <111251827+afaqir37@users.noreply.github.com>
2025-01-13 06:51:15 +0000

    Add countdown when the game is starting

--- Simon-Says/src/SimonSays.jsx
+++ Simon-Says/src/SimonSays.jsx
@@ -10,15 +10,51 @@ export default function SimonSays() {
 
   const [sequence, setSequence] = useState(generateRandomSequence())
   const [isPlaying, setIsPlaying] = useState(false)
+  const [countdown, setCountdown] = useState(4)
+  const [isVisible, setIsVisible] = useState(false)
   
 
+  useEffect(() => {
+
+      let timer
+
+      if (isPlaying && countdown !== 0) {
+        setIsVisible(true)
+        
+        timer = setInterval(() => {
+     
+           setCountdown(prev => {
+             if (prev > 0)
+               return prev - 1
+             else {
+               clearInterval(timer) 
+               return 0
+             }
+           })
+         },1000)
+
+      }
+      
+      let timeId
+      if (countdown === 0)
+        timeId = setTimeout(() => setIsVisible(false), 1000)
+
+
+      return () => {
+        clearInterval(timer)
+        clearTimeout(timeId)
+      }
+  }, [isPlaying, countdown])
   
 
 
   const startGame = () => {
     setSequence(generateRandomSequence())
     setIsPlaying(prev => !prev)
+    setCountdown(4)
+    setIsVisible(false)
   }
+
 
   return (
     <main className='flex flex-col items-center justify-start h-screen p-9 bg-[#E7FBE6]'>
@@ -32,6 +68,18 @@ export default function SimonSays() {
           {isPlaying ? 'Restart' : 'New Game'}
         </button>
       </div>
+
+
+      <div className='h-32'>
+
+        
+          <div className={`font-sans text-xl font-bold  transition-opacity duration-500 ease-in-out delay-200 ${isVisible ? 'opacity-100 visible' : 'opacity-0 invisible'} `}>
+            Memorize the following sequence in {' '}
+            <span>{countdown}</span>
+          </div>
+
+      </div>
+
 
       <div className='flex flex-col items-center'>
 
